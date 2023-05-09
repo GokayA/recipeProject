e273f704ce35a229001f3327c29c11a3edb61f40
yokyaa <arjenrobbennn@gmail.com>
2023-05-09 13:54:18 +0300

    Fixed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import RecipePage from '@/components/RecipePage';
 
 export default async function Home() {
   return (
-    <main className="bg-stone-300 ">
+    <main className="bg-stone-300">
       <RecipePage />
     </main>
   );
