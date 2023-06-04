e00667e5ed69bd8a7745e20fbecec2d467a5c7ca
yokyaa <arjenrobbennn@gmail.com>
2023-06-04 17:39:09 +0300

    added font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import { Analytics } from '@vercel/analytics/react';
 import { SessionProvider } from 'next-auth/react';
+import { Roboto } from 'next/font/google';
 import '../styles/globals.css';
 import Providers from './providers';
+
+const roboto = Roboto({ weight: '400', subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -12,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>
         <SessionProvider>
           <Providers>
