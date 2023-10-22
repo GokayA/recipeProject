e3f8690abad4a3c090bd1183168e962912775664
GokayA <arjenrobbennn@gmail.com>
2023-10-22 12:54:58 +0300

    added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import RecipePage from '@/components/RecipePage';
+import { constructMetadata } from '@/lib/utils';
 
-export const metadata = {
-  title: 'OnlyGoodRecipes',
-  description: 'The website to add recipes created with nextjs',
-};
-
+// export const metadata = {
+//   title: 'OnlyGoodRecipes',
+//   description: 'The website to add recipes created with nextjs',
+// };
+export const metadata = constructMetadata();
 export const revalidate = 0;
 
 export default async function Home() {
   return (
-    <main className="">
+    <main>
       <RecipePage />
     </main>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,51 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { Metadata } from 'next';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = 'OnlyGoodRecipes',
+  description = 'The website to add recipes created with nextjs',
+  image = 'public/logo.svg',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: any;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@Olurrolurrr',
+    },
+    icons,
+    metadataBase: new URL('https://recipe-project-omega.vercel.app'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
